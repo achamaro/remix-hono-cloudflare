80c46d9a1dc2da39462215aca135254f94e911d2
achamaro <a.achamaro@gmail.com>
2024-03-11 09:50:15 +0900

    chore: sentry

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,10 +3,6 @@ import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToReadableStream } from "react-dom/server";
 
-export function handleError(err: unknown) {
-  console.trace(err);
-}
-
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
